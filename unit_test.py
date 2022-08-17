2bc607f5c7a571ca87c1b8e6a83a7599f1f47b49
virginiap <luzanaperez@gmail.com>
2022-08-17 17:09:09 +0200

    refactoring unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -4,9 +4,12 @@ from main import *
 class TestPersonnage(unittest.TestCase):
     def setUp(self): 
         self.PV = 50
+        self.Pseudo = 'toto'
+        self.Force =  10
+        self.Armure =  5
     def test_personnage(self):
- #       result = self.assertTrue ( (self.PV).isnumber(), msg=None)
-        result = self.assertTrue (str(self.PV).startswith('50'))
+        result = personnage (self.Pseudo, self.PV, self.Force, self.Armure)
+        self.assertAlmostEqual(len(result), 4)
     
 if __name__ == "__main__":
     unittest.main()
