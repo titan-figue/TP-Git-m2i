09d9219765a7b4d34360eaa3b4df4ee91cc8d9d4
virginiap <luzanaperez@gmail.com>
2022-08-17 14:51:40 +0200

    c1 creation d'un personnage

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Creation d'un personnage
+
+# titre
+print ("Crée ton personnage !")
+def personnage (Pseudo, PV, Force, Armure):
+
+# creation du personnage
+
+    result = [Pseudo, PV, Force, Armure]
+    return result
+
+print (personnage ("Toto", 50, 9, "etoile"))
