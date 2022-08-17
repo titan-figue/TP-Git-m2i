0edd842c0066f16b1fef1c09dd6184105342397e
virginiap <luzanaperez@gmail.com>
2022-08-17 17:30:41 +0200

    update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -20,5 +20,15 @@ class GenerationDuMonstre(unittest.TestCase):
         test_name=result[0]
         self.assertRegex(test_name,r'[a-zA-Z0-9]')
 
-if __name__ == '__main__':
-    unittest.main()
+class TestPersonnage(unittest.TestCase):
+    def setUp(self): 
+        self.PV = 50
+        self.Pseudo = 'toto'
+        self.Force =  10
+        self.Armure =  5
+    def test_personnage(self):
+        result = personnage (self.Pseudo, self.PV, self.Force, self.Armure)
+        self.assertAlmostEqual(len(result), 4)
+    
+if __name__ == "__main__":
+    unittest.main()   
