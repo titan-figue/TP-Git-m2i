c933456b8395d81e272f7ae273fb0c7b957598c7
gregory <gnorvene@gmail.com>
2022-08-17 14:52:02 +0200

    add feature create monster

---
+++ main.py
@@ -0,0 +1,13 @@
+def function_creation_monstre(name) :
+    result =[name,5,5, 5]
+    return  result
+
+
+def function_generation_du_monstre() :
+    name  = input('Entrez le nom du monstre : ')
+    print ( name)
+    result = function_creation_monstre(name)
+
+    return  result
+
+
